3b4b02eceb25dfabf5d51077ad1501892a5c0c2e
wangchun <wangchun@ysbang.cn>
2018-09-30 14:47:35 +0800

    g

--- src/main/java/hello/DockerSbDemoApplication.java
+++ src/main/java/hello/DockerSbDemoApplication.java
@@ -15,7 +15,7 @@ public class DockerSbDemoApplication {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World231111 ffffffffdddgggg";
+        return "Hello  Docker World231111 ffffffffdddgggg & gecko!!!";
     }
 
 }
